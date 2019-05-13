978e732a83c56a80a5bc6c2225197fa8c667717f
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2019-05-13 21:09:45 +0800

    Updates

---
+++ precache-manifest.68e446fdb1fbcddb0bc313f38090d8d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1791973ae2dedfb16e480226048dc8a8",
+    "url": "/wtalk/index.html"
+  },
+  {
+    "revision": "5bad162a339b58b5509d",
+    "url": "/wtalk/static/css/main.1bd070f5.chunk.css"
+  },
+  {
+    "revision": "88502ca5d54ba54c5cd5",
+    "url": "/wtalk/static/js/2.029f5b06.chunk.js"
+  },
+  {
+    "revision": "5bad162a339b58b5509d",
+    "url": "/wtalk/static/js/main.fd1baca5.chunk.js"
+  },
+  {
+    "revision": "6d32df35a7aa99c5840b",
+    "url": "/wtalk/static/js/runtime~main.0c637cca.js"
+  },
+  {
+    "revision": "809d396d207ab2192d13f51ac5090f3f",
+    "url": "/wtalk/static/media/sampleUser1.809d396d.jpg"
+  },
+  {
+    "revision": "1f39568017062a6a025c7c48a9d544de",
+    "url": "/wtalk/static/media/sampleUser2.1f395680.jpg"
+  }
+]);
